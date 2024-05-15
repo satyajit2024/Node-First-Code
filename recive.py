57ca8d4e909cb679db5902cb11d7591160ff6128
satyajit2024 <satyajit.bariflo@outlook.com>
2024-05-15 18:12:06 +0530

    cur

--- recive.py
+++ recive.py
@@ -1,25 +1,20 @@
-import redis
-import time
-from LoRaRF import SX127x
-import os
-import sys
+import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(os.path.dirname(currentdir)))
+from LoRaRF import SX127x
+import time
+import redis
 
 
 def recive():
-    redis_server = redis.Redis(host="localhost", port=6379, db=0)
+    redis_server = redis.Redis(host="localhost",port=6379,db=0)
     # Begin LoRa radio and set NSS, reset, busy, IRQ, txen, and rxen pin with connected Raspberry Pi gpio pins
     # IRQ pin not used in this example (set to -1). Set txen and rxen pin to -1 if RF module doesn't have one
-    busId = 0
-    csId = 0
-    resetPin = 22
-    irqPin = -1
-    txenPin = -1
-    rxenPin = -1
+    busId = 0; csId = 0
+    resetPin = 22; irqPin = -1; txenPin = -1; rxenPin = -1
     LoRa = SX127x()
     print("Begin LoRa radio")
-    if not LoRa.begin(busId, csId, resetPin, irqPin, txenPin, rxenPin):
+    if not LoRa.begin(busId, csId, resetPin, irqPin, txenPin, rxenPin) :
         raise Exception("Something wrong, can't begin LoRa radio")
 
     # Set frequency to 915 Mhz
@@ -28,31 +23,23 @@ def recive():
 
     # Set RX gain. RX gain option are power saving gain or boosted gain
     print("Set RX gain to power saving gain")
-    # AGC on, Power saving gain
-    LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING, LoRa.RX_GAIN_AUTO)
+    LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING, LoRa.RX_GAIN_AUTO)    # AGC on, Power saving gain
 
     # Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
     # Receiver must have same SF and BW setting with transmitter to be able to receive LoRa packet
     print("Set modulation parameters:\n\tSpreading factor = 7\n\tBandwidth = 125 kHz\n\tCoding rate = 4/5")
-    # LoRa spreading factor: 7
-    LoRa.setSpreadingFactor(7)
-    # Bandwidth: 125 kHz
-    LoRa.setBandwidth(125000)
-    # Coding rate: 4/5
-    LoRa.setCodeRate(5)
+    LoRa.setSpreadingFactor(7)                                      # LoRa spreading factor: 7
+    LoRa.setBandwidth(125000)                                       # Bandwidth: 125 kHz
+    LoRa.setCodeRate(5)                                             # Coding rate: 4/5
 
     # Configure packet parameter including header type, preamble length, payload length, and CRC type
     # The explicit packet includes header contain CR, number of byte, and CRC type
     # Receiver can receive packet with different CR and packet parameters in explicit header mode
     print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
-    # Explicit header mode
-    LoRa.setHeaderType(LoRa.HEADER_EXPLICIT)
-    # Set preamble length to 12
-    LoRa.setPreambleLength(12)
-    # Initialize payloadLength to 15
-    LoRa.setPayloadLength(15)
-    # Set CRC enable
-    LoRa.setCrcEnable(True)
+    LoRa.setHeaderType(LoRa.HEADER_EXPLICIT)                        # Explicit header mode
+    LoRa.setPreambleLength(12)                                      # Set preamble length to 12
+    LoRa.setPayloadLength(15)                                       # Initialize payloadLength to 15
+    LoRa.setCrcEnable(True)                                         # Set CRC enable
 
     # Set syncronize word for public network (0x34)
     print("Set syncronize word to 0x34")
@@ -61,7 +48,7 @@ def recive():
     print("\n-- LoRa Receiver --\n")
 
     # Receive message continuously
-    while True:
+    while True :
 
         # Request for receiving new LoRa packet
         LoRa.request()
@@ -72,7 +59,7 @@ def recive():
         # read() and available() method must be called after request() or listen() method
         message = ""
         # available() method return remaining received payload length and will decrement each read() or get() method called
-        while LoRa.available() > 1:
+        while LoRa.available() > 1 :
             message += chr(LoRa.read())
         counter = LoRa.read()
 
@@ -81,20 +68,14 @@ def recive():
 
         message_list = message.split("/")
 
-        redis_server.set(
-            "current_r", message_list[0] if message_list[0] != None else 0)
-        redis_server.set(
-            "current_y", message_list[1]if message_list[1] != None else 0)
-        redis_server.set(
-            "current_b", message_list[2] if message_list[2] != None else 0)
+        redis_server.set("current_r",message_list[0] if message_list[0] != None else 0)
+        redis_server.set("current_y",message_list[1]if message_list[1] != None else 0)
+        redis_server.set("current_b",message_list[2] if message_list[2] != None else 0)
 
         # Print packet/signal status including RSSI, SNR, and signalRSSI
-        print("Packet status: RSSI = {0:0.2f} dBm | SNR = {1:0.2f} dB".format(
-            LoRa.packetRssi(), LoRa.snr()))
+        print("Packet status: RSSI = {0:0.2f} dBm | SNR = {1:0.2f} dB".format(LoRa.packetRssi(), LoRa.snr()))
 
         # Show received status in case CRC or header error occur
         status = LoRa.status()
-        if status == LoRa.STATUS_CRC_ERR:
-            print("CRC error")
-        elif status == LoRa.STATUS_HEADER_ERR:
-            print("Packet header error")
+        if status == LoRa.STATUS_CRC_ERR : print("CRC error")
+        elif status == LoRa.STATUS_HEADER_ERR : print("Packet header error")
